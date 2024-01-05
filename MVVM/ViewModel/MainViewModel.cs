5caa4cf1251efee5864f11124a60b96f35fdd476
Tahnee <95494849+SBMStudiosDesigns@users.noreply.github.com>
2024-01-05 09:16:44 -0500

    2nd commit
    Main menu changes usercontrol view. Added comments to explain methods being used

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -11,9 +11,13 @@ namespace ModernWPFDesign.MVVM.ViewModel
     {
         public RelayCommand HomeViewCommand { get; set; }
         public RelayCommand DiscoveryViewCommand { get; set; }
+        public RelayCommand FeaturedViewCommand { get; set; }
         public HomeViewModel HomeVM { get; set; }
         public DiscoveryViewModel DiscoveryVM { get; set; }
+        public FeaturedViewModel FeaturedVM { get; set; }
         private object _currentView;
+
+        // get+set current view
         public object CurrentView
         {
             get { return _currentView; }
@@ -23,15 +27,19 @@ namespace ModernWPFDesign.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        // change content view
         public MainViewModel()
         {
             HomeVM = new HomeViewModel();
             DiscoveryVM = new DiscoveryViewModel();
+            FeaturedVM = new FeaturedViewModel();
 
             CurrentView = HomeVM;
 
             HomeViewCommand = new RelayCommand(o => { CurrentView = HomeVM;});
             DiscoveryViewCommand = new RelayCommand(o => { CurrentView = DiscoveryVM;});
+            FeaturedViewCommand = new RelayCommand(o => { CurrentView = FeaturedVM;});
         }
     }
 }
