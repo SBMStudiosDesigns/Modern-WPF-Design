3ad83ef9246f676b90c451cf15be9f03003449ad
Tahnee <95494849+SBMStudiosDesigns@users.noreply.github.com>
2024-01-04 23:13:48 -0500

    1st Commit
    Modern WPF Design using:
    -ICommand
    -INotifiyPropertyChanged
    -DataBinding
    -Resource dictionaries & data templates

---
+++ Core/ObservableObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernWPFDesign.Core
+{
+    //INotifyPropertyChanged allows object to notify other objects(listeners) that its state has changed
+    class ObservableObjects : INotifyPropertyChanged //ObservableObjects inheritfrom the INotifyPropertyChanged class
+    {
+        //PropertyChangedEventHandler is a delegate has object & PropertyChangedEventArgs
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //method to change property
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            //if property is not null, change to new string being passed through
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name)); 
+        }
+    }
+}
